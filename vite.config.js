ef799e318e489c624e578a4b7c72e04c8686123c
Srinivas Harish <srinivas@dockware.ai>
2025-11-24 21:53:03 -0600

    Deploy fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  base: "",
   plugins: [react()],
-  base: "/srinivas-harish.github.io/"
 });
 
